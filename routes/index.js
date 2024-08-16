7f48dee545b7fb4a3cff33770f9bc29929704c9d
Scott Campion <scott.c@vyrida.com>
2024-08-16 10:18:46 -0400

    Initial

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+const router = Router()
+
+router.get('/', (req, res, next) => {
+  const template = 'home'
+  res.render(template, {
+    CLIENT_IP: undefined,
+    USRV_Host: 'bnkio46py0.execute-api.us-east-1.amazonaws.com/production/',
+    USRV_IP: undefined,
+    USRV_MsgPort: undefined,
+    USRV_Start: undefined,
+    // Introduced
+    USRV_WSS: true,
+    USRV_Preamble: '{"action":"join-network"}',
+    CDN: 'https://assets.turbo360.co/meme-test-td4gjz/static'
+  })
+})
+
+module.exports = router
