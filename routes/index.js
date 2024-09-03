b1f312a991473a1f1c7cede59d44e5dd79ab484c
Scott Campion <scott.c@vyrida.com>
2024-09-03 16:19:47 -0400

    feat: Support multiplexed web socket gateway via URL parameters.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const { Router } = require('express')
 const router = Router()
 
+// Environment parameters that reference the Turbo 360 Web Socket Gateway path
+// Note: no protocol prefix
+const gateway = process.env.WSG_GATEWAY 
+  ? process.env.WSG_GATEWAY
+  : 'q716d6lwhe.execute-api.us-east-1.amazonaws.com/production/';
+const projectSlug = process.env.TURBO_PROJECT_SLUG;
+
 router.get('/', (req, res, next) => {
   const template = 'home'
   res.render(template, {
     CLIENT_IP: undefined,
-    USRV_Host: 'bnkio46py0.execute-api.us-east-1.amazonaws.com/production/',
+    USRV_Host: `${gateway}?turbo=${projectSlug}`,
     USRV_IP: undefined,
     USRV_MsgPort: undefined,
     USRV_Start: undefined,
